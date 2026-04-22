6aec9adbc31b4959759eb5371fe1ab8fd6240f06
KhalidTheCoder <khalidahsan2407@gmail.com>
2026-04-22 15:11:29 +0600

    feat: add promo banner section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturesSection from "@/components/home/FeaturesSection";
 import HeroSection from "@/components/home/HeroSection";
+import PromoBanner from "@/components/home/PromoBanner";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <FeaturesSection />
+      <PromoBanner />
     </div>
   );
 }
