f73b6da602f6420350a1954f3875dec59e696aa3
KhalidTheCoder <khalidahsan2407@gmail.com>
2026-04-22 18:16:30 +0600

    added footer component on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/shared/Footer";
 import Navbar from "@/components/shared/Navbar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body className='min-h-full flex flex-col'>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
